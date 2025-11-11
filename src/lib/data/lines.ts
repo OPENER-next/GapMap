80063fd8ddfcb366f37956dafde79daa5e56c334
Robbendebiene <robbendebiene@mailbox.org>
2025-11-11 12:13:06 +0100

    Sort line data by name

--- src/lib/data/lines.ts
+++ src/lib/data/lines.ts
@@ -7,22 +7,25 @@ import {geoJSONLineData, geoJSONStopData} from "virtual:osm-data/data";
  * Generated example data from GeoJSON OSM extract.
  * See `tools` directory
  */
-export const lineData = new Map(geoJSONLineData.features.map(l => {
-  return [
-    l.id as string,
-    new Line({
-      id: l.id as string,
-      bbox: l.bbox as [number, number, number, number],
-      name: l.properties!.name as string,
-      stops: l.properties!._stops.map((id: string) => {
-        const stop = geoJSONStopData.features.find(f => f.id === id)!;
-        return new Platform({
-          id: stop.id as string,
-          coordinates: stop.geometry.coordinates as [number, number],
-          name: stop.properties!.name,
-          boardingHeight: stop.properties?.height,
-        })
-      }),
-    })
-  ]
-}));
+export const lineData = new Map(geoJSONLineData.features
+  .map<[string, Line]>(l => {
+    return [
+      l.id as string,
+      new Line({
+        id: l.id as string,
+        bbox: l.bbox as [number, number, number, number],
+        name: l.properties!.name as string,
+        stops: l.properties!._stops.map((id: string) => {
+          const stop = geoJSONStopData.features.find(f => f.id === id)!;
+          return new Platform({
+            id: stop.id as string,
+            coordinates: stop.geometry.coordinates as [number, number],
+            name: stop.properties!.name,
+            boardingHeight: stop.properties?.height,
+          })
+        }),
+      })
+    ]
+  })
+  .sort((a,b) => a[1].name.localeCompare(b[1].name))
+);
